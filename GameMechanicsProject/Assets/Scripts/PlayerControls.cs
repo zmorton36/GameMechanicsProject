ecd87be8bf72b3fa0b3f0de5ff99d429dfe18241
zmorton36 <zmorton36@gmail.com>
2019-01-15 19:40:06 -0500

    Player Movement & Jump
    - Added Player object to Playground scene
    -Added floor object to Playground scene
    -Added "Floor" tag
    -Renamed "PlayerControls" Class name from "PlayerMovement" to "PlayerControls"
    - Added Player Controls to Player Object
    -Changed speed to 3
    - Attached Main Camera to Player object
    - Renamed Main Camera to "PlayerCamera"
    - Created Prefabs folder
    -Created "Player" prefab

--- GameMechanicsProject/Assets/Scripts/PlayerControls.cs
+++ GameMechanicsProject/Assets/Scripts/PlayerControls.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerControls : MonoBehaviour
 {
 	private Rigidbody rb;
 	[SerializeField]
