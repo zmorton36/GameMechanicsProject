46e58cbcc338c49ed3036273936e8481da295534
AlexanderTess <46579182+AlexanderTess@users.noreply.github.com>
2019-01-22 16:05:43 -0500

    Updated Main Menu , and UI's for Credits and Level Select
    Created a Level Select and Credits Menu
    Got Main Menu Working properly

---
+++ GameMechanicsProject/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void loadscene2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void loadscene3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void loadscene4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void loadscene5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void loadscene6()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void loadscene7()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void loadscene8()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void loadscene9()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void loadscene10()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+    public void loadscene11()
+    {
+        SceneManager.LoadScene(11);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
