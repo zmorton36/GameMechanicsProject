bf480f8b30e717191f33fcc21367eadbf7bf4f4e
harpzone <teddyh789@gmail.com>
2019-01-29 14:50:57 -0500

    Fixed Pressure plate scirpt

--- GameMechanicsProject/Assets/Scripts/P_PlateTrigger.cs
+++ GameMechanicsProject/Assets/Scripts/P_PlateTrigger.cs
@@ -7,34 +7,35 @@ public class P_PlateTrigger : MonoBehaviour
     [SerializeField]
     private Animator PlateAnim, OtherAnim;
 
-    void OnCollisionEnter(Collision other)
+    void OnCollisionStay(Collision other)
     {
         IsHere = true;
 
-       if(IsHere == true && (other.gameObject.tag == "Grabbable" || other.gameObject.tag == "LittleBrother" || other.gameObject.tag == "BigBrother"))
-        {
-            
+		if (IsHere == true && (other.gameObject.tag == "Grabbable" || other.gameObject.tag == "LittleBrother" || other.gameObject.tag == "BigBrother"))
+		{
 
-            if(IsHere == true && PlateAnim.GetBool("PressureTrigger") == false)
-            {
-                PlateAnim.SetBool("PressureTrigger", true);
-                OtherAnim.SetBool("PlayAnim", true);
-            }
-            
-        }
+
+			if (IsHere == true && PlateAnim.GetBool("PressureTrigger") == false)
+			{
+				PlateAnim.SetBool("PressureTrigger", true);
+				OtherAnim.SetBool("PlayAnim", true);
+			}
+
+		}
+		else IsHere = false;
     }
 
-    void OnCollisionExit(Collision other)
-    {
-        if(IsHere == true)
-        {
-            IsHere = false;
+    //void OnCollisionExit(Collision other)
+    //{
+    //    if(IsHere == true)
+    //    {
+    //        IsHere = false;
             
-            if(IsHere == false && PlateAnim.GetBool("PressureTrigger") == true)
-            {
-                PlateAnim.SetBool("PressureTrigger", false);
-                OtherAnim.SetBool("PlayAnim", false);
-            }
-        }
-    }
+    //        if(IsHere == false && PlateAnim.GetBool("PressureTrigger") == true)
+    //        {
+    //            PlateAnim.SetBool("PressureTrigger", false);
+    //            OtherAnim.SetBool("PlayAnim", false);
+    //        }
+    //    }
+    //}
 }
